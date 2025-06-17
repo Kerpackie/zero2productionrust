862b4222a0f5b8415d22d4c66024e2b043932aa4
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2025-06-17 17:47:22 +0100

    Refactor to work with random port

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
+use std::net::TcpListener;
 
 /*async fn greet(req: HttpRequest) -> impl Responder{
     let name = req.match_info().get("name").unwrap_or("World");
@@ -8,17 +9,12 @@ use actix_web::dev::Server;
 }
 */
 async fn health_check(req: HttpRequest) -> impl Responder{
-    HttpResponse::Ok()
+    HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-        /*            .route("/", web::get().to(greet))
-                    .route("/{name}", web::get().to(greet))*/
-    })
-        .bind("127.0.0.1:8000")?
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
         .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,10 @@
+use std::fmt::format;
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
+    let address = spawn_app();
     spawn_app();
     // We need to bring in reqwest
     // to perform HTTP Requests against our application
@@ -9,7 +12,7 @@ async fn health_check_works() {
     
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -20,8 +23,15 @@ async fn health_check_works() {
 }
 
 // Launch application in background -- somehow.
-fn spawn_app() {
-    let server = zero2productionrust::run().expect("failed to bind address");
+fn spawn_app() -> String {
+    let tcp_listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+    
+    // Retrieve port assigned by the OS
+    let port = tcp_listener.local_addr().unwrap().port();
+
+    let server = zero2productionrust::run(tcp_listener).expect("failed to bind address");
     
     let _ = tokio::spawn(server);
+    
+    format!("http://127.0.0.1:{}", port)
 }
